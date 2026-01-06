8c6622b3478027f85dde08f51f671c0da6b804e7
“GodDamnGitHub” <jsczzzx@yahoo.com>
2026-01-06 01:07:32 -0500

    validation added

--- src/app/payment-element/payment-element.component.ts
+++ src/app/payment-element/payment-element.component.ts
@@ -20,6 +20,67 @@ export class PaymentElementComponent {
       securityCode: ['', Validators.required],
       postalCode: ['', Validators.required]
     })
+
+  this.paymentForm.get('cardNumber')?.valueChanges.subscribe(val => {
+    if (val) {
+      const digitsOnly = val.replace(/\D/g, '');
+
+      const formatted = digitsOnly
+        .match(/.{1,4}/g)
+        ?.join(' ') || '';
+
+      if (formatted !== val) {
+        this.paymentForm.get('cardNumber')?.setValue(formatted, { emitEvent: false });
+      }
+    }
+
+
+  });
+
+
+    this.paymentForm.get('expirationDate')?.valueChanges.subscribe(val=>{
+      if (val) {
+        const digitsOnly = val.replace(/\D/g, '');
+        
+        const formatted = digitsOnly
+          .match(/.{1,2}/g)
+          ?.join('/') || '';
+        
+        if (formatted !== val) {
+          this.paymentForm.get('expirationDate')?.setValue(formatted, { emitEvent: false });
+        }
+      }
+    })
+
+    this.paymentForm.get('securityCode')?.valueChanges.subscribe(val=>{
+      if (val) {
+        const digitOnly = val.replace(/\D/g, '');
+        if (digitOnly !== val) {
+          this.paymentForm.get('securityCode')?.setValue(digitOnly, {emitEvent: false});
+        }
+      }      
+    })
+
+    this.paymentForm.get('postalCode')?.valueChanges.subscribe(val=>{
+      if (val) {
+        const digitOnly = val.replace(/\D/g, '');
+        if (digitOnly !== val) {
+          this.paymentForm.get('postalCode')?.setValue(digitOnly, {emitEvent: false});
+        }
+      }      
+    })
+
+
+  }
+
+
+
+  addWhiteSpace(val: string) {
+    if (val) {
+      if (val.length === 4 || val.length === 9 || val.length === 14) {
+        this.paymentForm.get('cardNumber')?.setValue(val+' ', {emitEvent: false});
+      } 
+    }
   }
 
 }
