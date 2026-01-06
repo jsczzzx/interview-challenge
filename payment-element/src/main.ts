d15050c52ce5dfab314a695a3b65aefc339db4a0
“GodDamnGitHub” <jsczzzx@yahoo.com>
2026-01-06 12:28:22 -0500

    angular-elements-setup

--- payment-element/src/main.ts
+++ payment-element/src/main.ts
@@ -1,6 +1,17 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, createApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { createCustomElement } from '@angular/elements';
+import { PaymentElementComponent } from './app/payment-element/payment-element.component';
 
 bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
+
+createApplication(appConfig)
+  .then((app) => {
+    const paymentComponent = createCustomElement(PaymentElementComponent, {
+      injector: app.injector
+    });
+    customElements.define('payment-element', paymentComponent);
+  })
   .catch((err) => console.error(err));
